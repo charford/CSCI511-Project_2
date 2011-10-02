0c9a417b5f7af92329d8a9c1ad0db052e7cc7665
Casey Harford <casey@caseyharford.com>
2011-10-01 21:08:39 -0700

    added SiteDiagramGUI

---
+++ src/SiteDiagramGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+class SiteDiagramGUI extends JFrame {
+  public static void main (String [] args) {
+    System.out.println("SiteDiagramGUI");
+  JFrame f = new JFrame("A jframe");
+  f.setSize(250,250);
+  f.setLocation(300,200);
+  f.setVisible(true);
+  } 
+};
