f76ba2e4132e6e79d47796519a145b5b128aa104
Casey Harford <casey@caseyharford.com>
2011-10-03 23:29:41 -0700

    Jpanel

--- src/SiteDiagramGUI.java
+++ src/SiteDiagramGUI.java
@@ -1,10 +1,30 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Line2D;
+
 class SiteDiagramGUI extends JFrame {
+public void init() {
+  setBackground(Color.white);
+  setForeground(Color.white);
+}
   public static void main (String [] args) {
+    int gui_width = 50;
+    int gui_height = 50;
+    int cell_size = 15;
+    
     System.out.println("SiteDiagramGUI");
-  JFrame f = new JFrame("A jframe");
-  f.setSize(250,250);
-  f.setLocation(300,200);
-  f.setVisible(true);
+    JFrame f = new JFrame("Site Developer");
+    f.setSize(gui_width*cell_size,gui_height*cell_size);
+    f.setLocation(300,200);
+    f.setVisible(true);
   } 
+  public void paint(Graphics g) {
+    Graphics2D g2 = (Graphics2D) g;
+    g2.setPaint(Color.gray);
+    int x = 5;
+    int y = 7;
+    
+    g2.draw(new Line2D.Double(x,y,200,200));
+    g2.drawString("Line2D",x,250);
+  }
 };
