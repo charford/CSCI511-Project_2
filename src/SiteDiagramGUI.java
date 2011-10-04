ac9741b8f98807c2a55e5dfe4efc660398d7402d
Casey Harford <casey@caseyharford.com>
2011-10-03 23:59:24 -0700

    implemented JPanel, graph, and a new JPanel for a tools menu

--- src/SiteDiagramGUI.java
+++ src/SiteDiagramGUI.java
@@ -1,10 +1,38 @@
 import javax.swing.*;
-class SiteDiagramGUI extends JFrame {
-  public static void main (String [] args) {
-    System.out.println("SiteDiagramGUI");
-  JFrame f = new JFrame("A jframe");
-  f.setSize(250,250);
-  f.setLocation(300,200);
-  f.setVisible(true);
+import java.awt.*;
+class SiteDiagramGUI extends JPanel {
+  private static int fWidth,fHeight,cellSize;
+  public void paintComponent(Graphics g) {
+    System.out.println("start printComponent");
+  
+    int y=cellSize;
+    while(y<=fHeight*cellSize) {
+      g.drawLine(0,y,fWidth*cellSize,y);
+      y += cellSize;
+    }
+
+    int x=cellSize;
+    while(x<=fWidth*cellSize) {
+      g.drawLine(x,0,x,fHeight*cellSize);
+      x += cellSize;
+    }
+  
+    System.out.println("end printComponent");
   } 
+
+  public static void main (String [] args) {
+    fWidth = 20;
+    fHeight = 20;
+    cellSize = 35;
+    System.out.println("SiteDiagramGUI");
+    JFrame f = new JFrame();
+    f.add(new SiteDiagramGUI());
+    f.setSize(fWidth*cellSize,fHeight*cellSize);
+    f.setVisible(true);
+
+    JFrame t = new JFrame();
+    t.setSize(100,500);
+    t.setVisible(true);
+  }
+ 
 };
